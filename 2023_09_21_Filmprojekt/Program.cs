5f9113ac04d4183ad01052dd42661a9a7b0108f8
Fecske Márk <mrk.fecske@gmail.com>
2023-09-21 10:49:02 +0200

    beolvasas

--- 2023_09_21_Filmprojekt/Program.cs
+++ 2023_09_21_Filmprojekt/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,28 @@ namespace _2023_09_21_Filmprojekt
 {
     internal class Program
     {
+        static List<Film> filmek = new List<Film>();
         static void Main(string[] args)
         {
+            Fajlbeolvasasa("filmadatbazis.csv");
+            foreach (var item in filmek)
+            {
+                Console.WriteLine(item.Cim);
+            }
+        }
 
+        private static void Fajlbeolvasasa(string fajlNev)
+        {
+            using(StreamReader fileReader = new StreamReader(fajlNev))
+            {
+                fileReader.ReadLine();
+                while (!fileReader.EndOfStream)
+                {
+                    string sor = fileReader.ReadLine();
+                    Film film = new Film(sor);
+                    filmek.Add(film);
+                }
+            }
         }
     }
 }
